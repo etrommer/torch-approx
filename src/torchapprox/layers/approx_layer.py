427ab294afdef3a4b5a1ed0eb25d49793ff3e073
Elias Trommer <elias_trommer@posteo.de>
2022-08-30 15:02:01 +0200

    Move ApproxLayer configuration to separate class

--- src/torchapprox/layers/approx_layer.py
+++ src/torchapprox/layers/approx_layer.py
@@ -1,6 +1,8 @@
 # pylint: disable=missing-module-docstring
+import dataclasses
 import logging
 from abc import ABC, abstractmethod
+from typing import List
 
 import torch
 
@@ -9,6 +11,57 @@ from .approx_operator import LUT
 logger = logging.getLogger(__name__)
 
 SUPPORTED_TYPES = [torch.nn.Linear, torch.nn.Conv2d]
+
+
+@dataclasses.dataclass
+class ApproxLayerConfig:
+    """
+    Contains the mode configuration for the current layer.
+    Several paramters have to be set for inference and not all combinations are valid.
+    This class performs the input sanitization of the supplied mode string
+    and sets the internal parameters accordingly
+    """
+
+    quantize: bool = False
+    approximate: bool = False
+    add_noise: bool = False
+    _mode: str = "base"
+
+    @property
+    def mode(self) -> str:
+        """
+        Layer inference mode. Can be any of:
+        - `base`: Run inference as unperturbed FP32 baseline
+        - `quant`: Run inference using the layer's quantizer
+        - `approx`: Run inference using approximate product LUT
+        - `noise`: Run inference that is perturbed with additive Gaussian noise
+        """
+        return self._mode
+
+    @mode.setter
+    def mode(self, new_mode: str):
+        supported_modes: List[str] = ["base", "quant", "approx", "noise"]
+        if not new_mode in supported_modes:
+            raise ValueError(f"Trying to set unsupported mode: {new_mode}")
+        self._mode = new_mode
+
+        # Set correct configuration for current mode
+        # Reset current values to baseline
+        self.quantize = False
+        self.approximate = False
+        self.add_noise = False
+
+        if new_mode == "base":
+            return
+
+        # quantization is implicitly used for all modes
+        # except baseline
+        self.quantize = True
+
+        if new_mode == "noise":
+            self.add_noise = True
+        elif new_mode == "approx":
+            self.approximate = True
 
 
 class ApproxLayer(torch.nn.Module, ABC):
@@ -24,29 +77,11 @@ class ApproxLayer(torch.nn.Module, ABC):
         self.alpha_w: torch.Tensor = torch.Tensor([0.0])
         self.bitwidth: int = 8
         self.approx_op: LUT = LUT()
+        self.config: ApproxLayerConfig = ApproxLayerConfig()
 
         self._stdev: torch.nn.Paramter = torch.nn.Parameter(
             torch.tensor(0.0), requires_grad=True
         )
-        self._mode: str = "base"
-
-    @property
-    def mode(self) -> str:
-        """
-        Layer inference mode. Can be any of:
-        - `base`: Run inference as unperturbed FP32 baseline
-        - `quant`: Run inference using the layer's quantizer
-        - `approx`: Run inference using approximate product LUT
-        - `noise`: Run inference that is perturbed with additive Gaussian noise
-        """
-        return self._mode
-
-    @mode.setter
-    def mode(self, new_mode: str):
-        supported_modes = ["base", "quant", "approx", "noise"]
-        if not new_mode in supported_modes:
-            raise ValueError(f"Trying to set unsupported mode: {new_mode}")
-        self._mode = new_mode
 
     @property
     def int_max(self) -> int:
@@ -121,17 +156,17 @@ class ApproxLayer(torch.nn.Module, ABC):
             torch.abs(torch.min(self.weight)),  # type ignore
             torch.abs(torch.max(self.weight)),  # type ignore
         )
-        if self.mode == "approx":
+        if self.config.approximate:
             # approximate operation (always run in INT8 quant.)
             y = self.approx_fwd(x)
-        elif self.quantize:
+        elif self.config.quantize:
             # INT8 accurate operation
             y = self.quant_fwd(x)
         else:
             # FP32 accurate operation
             y = self.baseline_fwd(x)
 
-        if self.noise and self.training:
+        if self.config.add_noise and self.training:
             # Add noise to pre-activation output
             noise = torch.randn_like(y) * torch.std(y) * self.stdev
             y = y + noise
