81f4675900c2c34667020cc364b86fbd71dc0417
Elias Trommer <elias_trommer@posteo.de>
2022-09-02 16:31:12 +0200

    Add Approximate Linear layer

---
+++ src/torchapprox/layers/approx_linear.py
@@ -0,0 +1,70 @@
+# pylint: disable=missing-module-docstring
+import torch
+
+from .approx_layer import ApproxLayer
+
+
+class ApproxLinear(torch.nn.Linear, ApproxLayer):
+    """
+    Approximate Linear Layer implementation
+    """
+
+    def __init__(self, *args, **kwargs):
+        torch.nn.Linear.__init__(self, *args, **kwargs)
+        ApproxLayer.__init__(self)
+        self.opcount = torch.tensor(self.in_features * self.out_features).float()
+
+    @staticmethod
+    def from_super(cls_instance):
+        """
+        Alias for from_linear
+        """
+        return ApproxLinear.from_linear(cls_instance)
+
+    @staticmethod
+    def from_linear(linear):
+        """
+        Construct ApproxLinear from torch.nn.Linear layer
+        """
+        has_bias = linear.bias is not None
+        approx_instance = ApproxLinear(
+            linear.in_features, linear.out_features, bias=has_bias
+        )
+
+        with torch.no_grad():
+            approx_instance.weight = linear.weight
+            if has_bias:
+                approx_instance.bias = linear.bias
+
+        return approx_instance
+
+    @property
+    def fan_in(self) -> int:
+        return self.in_features
+
+    def baseline_fwd(self, x):
+        return torch.nn.functional.linear(x, self.weight)
+
+    def quant_fwd(self, x):
+        x_q = self.x_quantizer.fake_quant(x)
+        w_q = self.w_quantizer.fake_quant(self.weight)
+        y = torch.nn.functional.linear(x_q, w_q)
+        return y
+
+    def approx_fwd(self, x):
+
+        # Quantize to Int8 range
+        x_q = self.x_quantizer.quantize(x)[:, None, :]
+        w_q = self.w_quantizer.quantize(self.weight)
+
+        # ApproxGeMM
+        y = self.approx_op(x_q, w_q.T)
+        # Rescale results
+        y /= self.x_quantizer.scale_factor * self.w_quantizer.scale_factor
+
+        y = y.view(y.size(0), -1)
+        return y
+
+    # pylint: disable=arguments-renamed
+    def forward(self, x):
+        return ApproxLayer.forward(self, x)
--- test/test_approx_layer.py
+++ test/test_approx_layer.py
@@ -1,8 +1,73 @@
-import pytest
+import copy
 
-from torchapprox.layers import ApproxLayer
+import numpy as np
+import pytest
+import torch
+
+from torchapprox.layers import ApproxLayer, ApproxLinear, InferenceMode
 
 
 def test_instantiate():
     with pytest.raises(TypeError):
         al = ApproxLayer()
+
+
+def test_linear_from_super(device):
+    l = torch.nn.Linear(12, 8, device=device)
+    al = ApproxLinear.from_super(l)
+    assert l.weight.device == al.weight.device
+    assert l.in_features == al.in_features
+    assert l.out_features == al.out_features
+    assert torch.allclose(l.weight, al.weight)
+    assert torch.allclose(l.bias, al.bias)
+
+    x = torch.rand((4, 12))
+    assert torch.allclose(l(x), al(x))
+
+
+def test_linear_properties():
+    al = ApproxLinear(10, 20, False)
+    assert al.fan_in == 10
+    assert al.opcount == 200
+
+
+def test_linear_fwd(lut, device):
+    layer = ApproxLinear(20, 10, device=device)
+    layer.inference_mode = InferenceMode.APPROXIMATE
+    layer.approx_op.lut = lut
+
+    ref_layer = copy.deepcopy(layer)
+    ref_layer.inference_mode = InferenceMode.QUANTIZED
+
+    x = torch.rand((4, 20), device=device)
+
+    assert torch.allclose(ref_layer(x), layer(x))
+
+    layer.approx_op.lut = None
+    assert torch.allclose(ref_layer(x), layer(x))
+
+
+def test_linear_empty_lut(device):
+    layer = ApproxLinear(20, 10, bias=False, device=device)
+    layer.inference_mode = InferenceMode.APPROXIMATE
+    layer.approx_op.lut = np.zeros((256, 256))
+
+    x = torch.rand((4, 20), device=device)
+
+    res = layer(x)
+    assert torch.allclose(torch.zeros_like(res), res)
+
+
+def test_linear_noise(device):
+    layer = ApproxLinear(20, 10, device=device)
+    layer.inference_mode = InferenceMode.NOISE
+
+    ref_layer = copy.deepcopy(layer)
+    ref_layer.inference_mode = InferenceMode.QUANTIZED
+
+    x = torch.rand((4, 20), device=device)
+
+    layer.stdev = 0.1
+    assert not torch.allclose(layer(x), ref_layer(x))
+    layer.stdev = 0.0
+    assert torch.allclose(layer(x), ref_layer(x))
